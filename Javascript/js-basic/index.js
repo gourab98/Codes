1b03d05ac015442b3ed70219f279e6e59282385c
gourab98 <gourabsahag@gmail.com>
2021-12-09 15:31:59 +0600

    Java Scripts

---
+++ Javascript/js-basic/index.js
@@ -0,0 +1,10 @@
+        // This is my code.
+        console.log('Hello, World!');
+        let nam = 'Hello World!!';
+        console.log(nam);
+
+        let a=7;
+        const multByTen = (a) => a*10;
+        let result = multByTen(a);
+
+        console.log(result);
