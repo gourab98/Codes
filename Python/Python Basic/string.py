8312b8923a22ed7e19cb251e3bcd5af78d98836b
gourab98 <gourabsahag@gmail.com>
2022-10-10 19:14:23 +0600

    Update

--- Python/Python Basic/string.py
+++ Python/Python Basic/string.py
@@ -1,1 +1,16 @@
+### Formatting
+
 print('I love this course. (upper)'.upper())
+print('I love this course. (rjust 20)'.rjust(20))
+print('we love this course. (capitalize)'.capitalize())
+print('        I love this course. (strip)'.strip())
+
+### Concatenation
+
+# print('I like '+ str(cs_class_code)+ ' a lot!')
+print(f'{print} (print a function)')
+print(f'{type(229)} (print a type)')
+
+### Formatting
+
+# print('Old school formatting: {.2F}'.format(1.358))
