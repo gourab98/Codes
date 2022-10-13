a0b29f107c5a50a9ab81a3d485fa7b465222cca5
gourab98 <gourabsahag@gmail.com>
2022-10-13 20:28:49 +0600

    Added Nested Object

---
+++ Nodejs/basic/nestedObject.js
@@ -0,0 +1,13 @@
+var myStroage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+console.log(myStroage.car["outside"])
---
+++ Nodejs/basic/nestedObject2.js
@@ -0,0 +1,26 @@
+var myPlants = [
+    {
+        type: "flower",
+        list: [
+            "Tose",
+            "Marigold",
+            "Jasmin",
+            "Lily",
+            "Tulip"
+        ]
+    },
+    {
+        type: "fruit",
+        List: [
+            "Mango",
+            "Lichi",
+            "Banana",
+            "Jack-fruit",
+            "Orange",
+            "Apple",
+            "Guavara"
+        ]
+    }    
+]
+
+console.log(myPlants[1].List[3])
