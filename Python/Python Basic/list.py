44b9d28bca4a02928ba500dc70d046624dcd20cf
gourab98 <gourabsahag@gmail.com>
2022-10-13 11:47:52 +0600

    node.js http connection

---
+++ Python/Python Basic/list.py
@@ -0,0 +1,16 @@
+### List Creation
+
+list_1 = ['one','two','three']
+
+list_1.append(49)
+list_1.insert(0,'zero');
+
+# print(list_1)
+
+
+### Insertion
+
+list_2 = [1,2,3]
+list_1.extend(list_2)
+
+print(list_1)
