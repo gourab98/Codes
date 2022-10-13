1fbb01b26d9f6479a96c052dbb4daea155902f77
gourab98 <gourabsahag@gmail.com>
2022-10-13 16:53:00 +0600

    adding fuctions

---
+++ Nodejs/test/subFunction.js
@@ -0,0 +1,3 @@
+exports.sub = function sub(a,b){
+    return a-b;
+}
---
+++ Nodejs/test/testFunction.js
@@ -0,0 +1,8 @@
+var calAdding = require('./addFunction.js')
+var calSubstruction = require('./subFunction')
+
+
+var result = calAdding.adding(3.44,3.44)
+
+console.log("The output is: "+ result)
+console.log("The output of sub 'a' and 'b' is "+ calSubstruction.sub(23,5))
