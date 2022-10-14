364b5d5ab80792d37ee8f7b9a5b6483d21712863
gourab98 <gourabsahag@gmail.com>
2022-10-15 00:37:47 +0600

    add htmlpage

---
+++ Nodejs/htmlPage/app.js
@@ -0,0 +1,14 @@
+var http = require('http')
+var fs = require('fs')
+
+
+
+var server = http.createServer(function(req,res){
+    console.log('Request was made from: '+ req.url)
+    res.writeHead(200,{'Content-Type':'text/html'})
+    var myReadStream = fs.createReadStream('index.html','utf8')
+    myReadStream.pipe(res)
+})
+
+server.listen(3030,'127.0.0.1')
+console.log("Yes, You are online on port 3030 and address 127.0.0.1")
