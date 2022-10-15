fd8e7626ad8b0c30af9ad60ea6a2438b1f44cebe
gourab98 <gourabsahag@gmail.com>
2022-10-16 00:56:18 +0600

    Post Request in Node

--- Nodejs/express/app.js
+++ Nodejs/express/app.js
@@ -1,5 +1,8 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 app.set('view engine','ejs')
 
@@ -10,8 +13,19 @@ app.get('/', function(req,res){
 
 app.get('/contact', function(req,res){
     //res.send('This is a contactpage.');
-    res.sendFile(__dirname+'/contact.html')
+    //res.sendFile(__dirname+'/contact.html')
+    res.render('contact',{qs: req.query})
+    //console.log(req.query)
 })
+
+
+app.post('/contact', urlencodedParser, function(req,res){
+    console.log(req.body)
+    res.render('contact-success',{data: req.body})
+    //res.render('contact',{qs: req.query})
+    //console.log(req.query)
+})
+
 
 // using id:
 app.get('/profile/:name',function(req,res){
