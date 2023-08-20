5abbe7022e166dff9c5b5c4a45d59483100cc24c
gourab98 <gourabsahag@gmail.com>
2023-08-20 16:46:37 +0600

    nothing important

--- Python/AutoCommentBot/autocomment.py
+++ Python/AutoCommentBot/autocomment.py
@@ -4,7 +4,7 @@ import time
 comments = ["Hello, It is just an experiment.","So be relax, It is just for fun, be patient.","BTW, it is funny!","Do you still think, I am bot!. No, I am not.","It is normal and last of all","How are You Right now?","Tell me."]
 time.sleep(5)
 
-for i in range(21):
+for i in range(22):
     pyautogui.typewrite(comments[i%7])
     pyautogui.typewrite("\n")
     time.sleep(2)
