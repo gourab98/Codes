5570e4345d5e720963864d073658edf68ed7058b
gourab98 <gourabsahag@gmail.com>
2023-09-04 15:30:37 +0600

    updated

--- Javascript/js-basic/index.js
+++ Javascript/js-basic/index.js
@@ -1,10 +1,1 @@
-        // This is my code.
-        console.log('Hello, World!');
-        let nam = 'Hello World!!';
-        console.log(nam);
-
-        let a=7;
-        const multByTen = (a) => a*10;
-        let result = multByTen(a);
-
-        console.log(result);
+console.log('Hello, World!');
